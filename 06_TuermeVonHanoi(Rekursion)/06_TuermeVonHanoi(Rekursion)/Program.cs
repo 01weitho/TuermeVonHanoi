f2f8fba0ce375b007a34262add2459ad0ed6e60b
01weitho <01weitho@users.noreply.github.com>
2020-05-05 16:46:38 +0200

    Eigabe programmiert; eingabe von Zielturm mascht noch Probleme

--- 06_TuermeVonHanoi(Rekursion)/06_TuermeVonHanoi(Rekursion)/Program.cs
+++ 06_TuermeVonHanoi(Rekursion)/06_TuermeVonHanoi(Rekursion)/Program.cs
@@ -10,7 +10,40 @@ namespace _06_TuermeVonHanoi_Rekursion_
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Der Algorithmus soll n Scheiben vom Turm A nach Turm C ueber den Turm B schieben.
+            //Also muessen zuerst n-1 Scheiben von A nach B ueber C geschoben werden
+            //Dann wird die Scheibe von A nach C geschoben
+            //Als naechstes n-1 Scheiben vom Turm B nach Turm C ueber Turm A
+            Console.WriteLine("Wieviele Scheiben möchten Sie verschieben?\n");
+            uint AnzahlScheiben = 0;
+            while (!(uint.TryParse(Console.ReadLine(), out AnzahlScheiben)))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Eingabe ungueltig, nochmals versuchen.");
+                Console.ForegroundColor = ConsoleColor.White;
+            }
+
+            Console.WriteLine("Benennen Sie den Startturm: (A,B oder C)\n");
+            string Startturm =Console.ReadLine();
+            while (!(Startturm == "A" || Startturm == "B" || Startturm == "C"))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Eingabe ungueltig: Es ist nur A,B oder C zulaessig!");
+                Startturm = Console.ReadLine();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.WriteLine("\nBenennen Sie den Zielturm: (A,B oder C\n");
+            string Zielturm = Console.ReadLine();
+            while (!(Zielturm == "A" || Zielturm == "B" || Zielturm == "C") && Zielturm == Startturm)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Eingabe ungueltig nur A,B oder C zulässig außer dem Startturm({0})", Startturm);
+                Zielturm = Console.ReadLine();
+            }
+            Console.ForegroundColor = ConsoleColor.White;
+
+            Console.ReadKey();
         }
     }
 }
