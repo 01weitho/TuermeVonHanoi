adfdc5d980860c3168df45a67e798fb2b4d6c886
01weitho <01weitho@users.noreply.github.com>
2020-05-05 15:19:10 +0200

    Projekt erstellt; noch nichts programmiert

---
+++ 06_TuermeVonHanoi(Rekursion)/06_TuermeVonHanoi(Rekursion)/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+//Thomas Weithaler
+//4 BEL
+//05.05.2020
+//Tuerme von Hanoi
+
+namespace _06_TuermeVonHanoi_Rekursion_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
