0aec4a8c6a6bdac5e79e627d51d7840ca1924f30
01weitho <01weitho@users.noreply.github.com>
2020-05-06 09:21:46 +0200

    Eingabe abgeschlossen; als neachstes ist der algorythmus zu programmieren

--- 06_TuermeVonHanoi(Rekursion)/06_TuermeVonHanoi(Rekursion)/Program.cs
+++ 06_TuermeVonHanoi(Rekursion)/06_TuermeVonHanoi(Rekursion)/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading.Tasks;
 //Thomas Weithaler
 //4 BEL
 //05.05.2020
@@ -14,16 +15,16 @@ namespace _06_TuermeVonHanoi_Rekursion_
             //Also muessen zuerst n-1 Scheiben von A nach B ueber C geschoben werden
             //Dann wird die Scheibe von A nach C geschoben
             //Als naechstes n-1 Scheiben vom Turm B nach Turm C ueber Turm A
-            Console.WriteLine("Wieviele Scheiben möchten Sie verschieben?\n");
+            Console.WriteLine("Wieviele Scheiben möchten Sie verschieben?");
             uint AnzahlScheiben = 0;
             while (!(uint.TryParse(Console.ReadLine(), out AnzahlScheiben)))
             {
                 Console.ForegroundColor = ConsoleColor.Red;
-                Console.WriteLine("Eingabe ungueltig, nochmals versuchen.");
+                Console.WriteLine("Eingabe ungueltig, nur natuerliche Zahlen zulaessig.");
                 Console.ForegroundColor = ConsoleColor.White;
             }
 
-            Console.WriteLine("Benennen Sie den Startturm: (A,B oder C)\n");
+            Console.WriteLine("Benennen Sie den Startturm: (A,B oder C)");
             string Startturm =Console.ReadLine();
             while (!(Startturm == "A" || Startturm == "B" || Startturm == "C"))
             {
@@ -33,15 +34,66 @@ namespace _06_TuermeVonHanoi_Rekursion_
             }
             Console.ForegroundColor = ConsoleColor.White;
 
-            Console.WriteLine("\nBenennen Sie den Zielturm: (A,B oder C\n");
+            Console.WriteLine("Benennen Sie den Zielturm: (A,B oder C");
             string Zielturm = Console.ReadLine();
-            while (!(Zielturm == "A" || Zielturm == "B" || Zielturm == "C") && Zielturm == Startturm)
+            while (!(Zielturm == "A" || Zielturm == "B" || Zielturm == "C") || Zielturm == Startturm)
             {
                 Console.ForegroundColor = ConsoleColor.Red;
                 Console.WriteLine("Eingabe ungueltig nur A,B oder C zulässig außer dem Startturm({0})", Startturm);
                 Zielturm = Console.ReadLine();
             }
             Console.ForegroundColor = ConsoleColor.White;
+
+            string ueber = string.Empty;
+            switch (Startturm)
+            {
+                case "A":
+                    switch (Zielturm)
+                    {
+                        case "B":
+                            ueber = "C";
+                            break;
+
+                        case "C":
+                            ueber = "B";
+                            break;
+                    }
+                    break;
+
+                case "B":
+                    switch (Zielturm)
+                    {
+                        case "A":
+                            ueber = "C";
+                            break;
+
+                        case "C":
+                            ueber = "A";
+                            break;
+                    }
+                    break;
+
+                case "C":
+                    switch (Zielturm)
+                    {
+                        case "A":
+
+
+
+                            ueber = "B";
+                            break;
+
+                        case "B":
+                            ueber = "A";
+                            break;
+                    }
+                    break;
+                    
+            }
+
+            Console.WriteLine("Damit werden {0} Scheiben von Turm {1} nach Turm {2} ueber Turm {3} geschoben...", AnzahlScheiben, Startturm, Zielturm, ueber);
+
+            System.Threading.Thread.Sleep(100);
 
             Console.ReadKey();
         }
