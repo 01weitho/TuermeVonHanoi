aa3c70c90115d9cdd92802233f1d5d9114f101ae
01weitho <01weitho@users.noreply.github.com>
2020-05-06 10:14:53 +0200

    Angefangen den algorythmus zu programmieren

--- 06_TuermeVonHanoi(Rekursion)/06_TuermeVonHanoi(Rekursion)/Program.cs
+++ 06_TuermeVonHanoi(Rekursion)/06_TuermeVonHanoi(Rekursion)/Program.cs
@@ -9,12 +9,9 @@ namespace _06_TuermeVonHanoi_Rekursion_
 {
     class Program
     {
+        static int[,] Feld;
         static void Main(string[] args)
         {
-            //Der Algorithmus soll n Scheiben vom Turm A nach Turm C ueber den Turm B schieben.
-            //Also muessen zuerst n-1 Scheiben von A nach B ueber C geschoben werden
-            //Dann wird die Scheibe von A nach C geschoben
-            //Als naechstes n-1 Scheiben vom Turm B nach Turm C ueber Turm A
             Console.WriteLine("Wieviele Scheiben möchten Sie verschieben?");
             uint AnzahlScheiben = 0;
             while (!(uint.TryParse(Console.ReadLine(), out AnzahlScheiben)))
@@ -25,7 +22,7 @@ namespace _06_TuermeVonHanoi_Rekursion_
             }
 
             Console.WriteLine("Benennen Sie den Startturm: (A,B oder C)");
-            string Startturm =Console.ReadLine();
+            string Startturm = Console.ReadLine();
             while (!(Startturm == "A" || Startturm == "B" || Startturm == "C"))
             {
                 Console.ForegroundColor = ConsoleColor.Red;
@@ -85,14 +82,49 @@ namespace _06_TuermeVonHanoi_Rekursion_
                             break;
                     }
                     break;
-                    
-            }
 
+            }
+            Feld = new int[2, AnzahlScheiben];
             Console.WriteLine("Damit werden {0} Scheiben von Turm {1} nach Turm {2} ueber Turm {3} geschoben...", AnzahlScheiben, Startturm, Zielturm, ueber);
 
             System.Threading.Thread.Sleep(100);
 
+            //Anzeigen der Tuerme
+
+
+
             Console.ReadKey();
+        }
+
+        //Der Algorithmus soll n Scheiben vom Turm A nach Turm C ueber den Turm B schieben.
+        //Also muessen zuerst n-1 Scheiben von A nach B ueber C geschoben werden
+        //Dann wird die Scheibe von A nach C geschoben
+        //Als naechstes n-1 Scheiben vom Turm B nach Turm C ueber Turm A
+        
+        /// <summary>
+        /// Schiebt Scheiben vom Startturm zum Zielturm
+        /// </summary>
+        /// <param name="Start">Starttum, wo die Scheiben am Anfang liegen</param>
+        /// <param name="Ziel">Zielturm, wo die Scheiben zum Schluss liegen sollen</param>
+        /// <param name="Ueber">Der Turm wo die Scheiben "zwischengleagert" werden</param>
+        /// <param name="Anzahl">Anzahl der zu verschiebenden Scheiben</param>
+        static void verschiebe(string Start, string Ziel, string Ueber, uint Anzahl)
+        {
+            if (Anzahl == 1)
+            {
+                //Schiebe eine Scheibe von Start nach Ziel.
+            }
+            else
+            {
+                verschiebe(Start, Ueber, Ziel, Anzahl - 1);
+                //Schiebe eine Scheibe von Start nach Ziel
+                verschiebe(Ueber, Ziel, Start, Anzahl - 1);
+            }
+        }
+
+        static void schiebeEinTurm()
+        {
+               
         }
     }
 }
